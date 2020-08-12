c02e292ab423d810fd21b0fb05dcd66413d62e3a
Мариана Христозкова <kukita_new@teacher.bg>
2020-08-12 09:45:57 +0300

    Add project files.

---
+++ Tema1-Zadacha/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema1_Zadacha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
